2bd8bfa26e35c27698c921dd05dfe8236237d958
jagm <jagiela.mateusz@gmail.com>
2014-05-31 21:49:28 +0200

    add geb example from internet for google & wikipedia

---
+++ geb/src/test/resources/GebConfig.groovy
@@ -0,0 +1,10 @@
+/*
+	This is the Geb configuration file.
+	
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
+
+import org.openqa.selenium.firefox.FirefoxDriver
+
+// See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
+driver = { new FirefoxDriver() }
